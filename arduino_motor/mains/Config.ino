c1c33067ab0f0e9adbb3015eae4c8701f5998ba7
Zhang Danyang <zhangdanyangg@gmail.com>
2014-04-09 17:31:41 +0800

    turning low voltage

--- arduino_motor/mains/Config.ino
+++ arduino_motor/mains/Config.ino
@@ -23,6 +23,6 @@ bool Config::verbose = true;
 //double const Config::TICKS_PER_DEGREE_LEFT = (WHEELS_INTERVAL_LEFT/2)*(PI/180)*(1/DISTANCE_PER_TICK_CM); 
 
 // range from 796 - 810; 803 baseline, -7 to +7; log 14 = 4
-double const Config::TICKS_PER_DEGREE_LEFT = (803+(-4))/90.0; // [-6, -6]
-double const Config::TICKS_PER_DEGREE = (803+(+2))/90.0; // [-5, -4]  
+double const Config::TICKS_PER_DEGREE_LEFT = (803+(2))/90.0; // {6.08: [+2], 6.20: [-6]}
+double const Config::TICKS_PER_DEGREE = (803+(+2))/90.0; // {6.08: [+2], 6.20: [-5]}
 int const Config::VIRTUAL_OFFSET = 5;
